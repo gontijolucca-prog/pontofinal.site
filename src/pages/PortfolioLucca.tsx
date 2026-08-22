b2e574c9b3ec993bf102f12d2cd7517c5c3abd72
gontijolucca-prog <gontijolucca@gmail.com>
2026-08-22 10:24:16 +0100

    Portfólio Lucca: anel dourado com animação shining + selo de verificado dourado no avatar

--- src/pages/PortfolioLucca.tsx
+++ src/pages/PortfolioLucca.tsx
@@ -114,6 +114,9 @@ export default function PortfolioLucca() {
                 <div className="pf-avatar-inner">
                   <img src="/lucca-portfolio/lucca.png" alt="Lucca Gontijo" />
                 </div>
+                <div className="pf-verified" aria-label="Verificado">
+                  <svg viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/></svg>
+                </div>
               </div>
             </div>
             <div>
